a4afd70ccf3a3a52cb00918721e0a3629e3a5b63
fcalve <freddycalve@gmail.com>
2025-09-01 18:48:00 -0400

    initial commit

---
+++ src/ICMCalculator.java
@@ -0,0 +1,18 @@
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author fredd
+ *  @version Sep 1, 2025
+ */
+public class ICMCalculator
+{
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+
+}
---
+++ src/ICMCalculatorMain.java
@@ -0,0 +1,28 @@
+// -------------------------------------------------------------------------
+/**
+ *  Write a one-sentence summary of your class here.
+ *  Follow it with additional details about its purpose, what abstraction
+ *  it represents, and how to use it.
+ * 
+ *  @author fredd
+ *  @version Aug 30, 2025
+ */
+public class ICMCalculatorMain
+{
+
+
+    //~ Fields ................................................................
+
+    //~ Constructors ..........................................................
+
+    //~Public  Methods ........................................................
+    /**
+     * Main function
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+}
