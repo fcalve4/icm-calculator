db4c39b93a176a046d5ebc03c66fd2bd7bd17838
fcalve <freddycalve@gmail.com>
2026-01-20 09:06:31 -0500

    created some tests and added basic file input handling and console output handling for main function

--- src/ICMCalculatorMain.java
+++ src/ICMCalculatorMain.java
@@ -1,6 +1,10 @@
 // -------------------------------------------------------------------------
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileWriter;
 
 /**
  *  Main function for ICMCalculator
@@ -16,10 +20,46 @@ public class ICMCalculatorMain
      * Main function
      * @param args
      */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {  
+        if (args.length != 1) {
+            System.out.println("Invalid arguments. Try again.");
+            System.out.println(Arrays.toString(args));
+            return;
+        }
         
-     
+        File file = null; 
+        try {
+            file = new File(args[0]);
+            
+            Scanner sc = new Scanner(file);
+            
+            // first array
+            String[] stackTokens = sc.nextLine().trim().split("[,\\s]+"); // regex delimiter
+            int[] stacks = new int[stackTokens.length];
+            for (int i = 0; i < stackTokens.length; i++) {
+                stacks[i] = Integer.parseInt(stackTokens[i]);
+            }
+
+            // second array
+            String[] payoutTokens = sc.nextLine().trim().split("[,\\s]+");
+            double[] payouts = new double[payoutTokens.length];
+            for (int i = 0; i < payoutTokens.length; i++) {
+                payouts[i] = Double.parseDouble(payoutTokens[i]);
+            }
+            
+            sc.close();
+         
+            ICMCalculator calculator = new ICMCalculator(stacks, payouts);
+            double[] equities = calculator.calculate();
+            
+            
+            System.out.println(Arrays.toString(equities));
+           
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("Invalid file.");
+            e.printStackTrace();
+        }
 
     }
 }
--- src/ICMCalculatorTest.java
+++ src/ICMCalculatorTest.java
@@ -1,5 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
 /**
  * Unit tests for ICMCalculator.
@@ -10,23 +11,54 @@ import org.junit.jupiter.api.Test;
  */
 public class ICMCalculatorTest
 {
+    @Test
+    public void test3stacks1payout() {
+        int[] stacks = {500, 300, 200};
+        double[] payouts = {100.0};
+
+        ICMCalculator calc = new ICMCalculator(stacks, payouts);
+        double[] ev = calc.calculate();
+
+        System.out.println(Arrays.toString(ev));
+
+        assertEquals(50, ev[0], 1e-2);
+        assertEquals(30, ev[1], 1e-2);
+        assertEquals(20, ev[2], 1e-2);
+    }
     
     @Test
-    public void testCalculator() {
+    public void test3stacks2payouts() {
         int[] stacks = {500, 300, 200};
         double[] payouts = {70.0, 30.0};
 
         ICMCalculator calc = new ICMCalculator(stacks, payouts);
         double[] ev = calc.calculate();
-        
-        for (double e : ev) {
-            System.out.println(e); 
-        }
-        
 
-        assertEquals(45.17, ev[0], 1e-6);
-        assertEquals(32.25, ev[1], 1e-6);
-        assertEquals(22.57, ev[2], 1e-6);
+        System.out.println(Arrays.toString(ev));
+
+        assertEquals(45.17, ev[0], 1e-2);
+        assertEquals(32.25, ev[1], 1e-2);
+        assertEquals(22.57, ev[2], 1e-2);
+    }
+    
+    @Test
+    public void test3stacks3payouts() {
+        int[] stacks = {10000, 5000, 3000};
+        double[] payouts = {100.0, 30.0, 20.0};
+
+        ICMCalculator calc = new ICMCalculator(stacks, payouts);
+        double[] ev = calc.calculate();
+
+        System.out.println(Arrays.toString(ev));
+
+        assertEquals(67.69, ev[0], 1e-2);
+        assertEquals(46.25, ev[1], 1e-2);
+        assertEquals(36.06, ev[2], 1e-2);
+    }
+    
+    @Test
+    public void testMain() {
+        
     }
 
 }
