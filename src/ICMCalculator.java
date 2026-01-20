5faa4de4dd1cc4fcb11f10b63ba11bc6fb4c6360
fcalve <freddycalve@gmail.com>
2026-01-20 09:06:04 -0500

    completed logic for ICM calculations

--- src/ICMCalculator.java
+++ src/ICMCalculator.java
@@ -1,6 +1,3 @@
-import java.util.HashMap;
-import java.util.Map;
-
 // -------------------------------------------------------------------------
 /**
  * ICM Calculator. Computes each player's expected equity share of a tournament
@@ -13,12 +10,12 @@ public class ICMCalculator
 {
     private int[] stacks; // expressed as # of chips integer
     private double[] payouts; // expressed as a $ integer
-    int numStacks;
-    int numPayouts;
+    private int numStacks;
+    private int numPayouts;
 
     // ----------------------------------------------------------
     /**
-     * Create a new ICMCalculator object.
+     * Create a new ICMCalculator
      * @param stacks List of player chip stacks
      * @param payouts List of payouts
      */
@@ -34,23 +31,23 @@ public class ICMCalculator
     }
     
     /**
-     * Recursive get equity method
+     * Recursive function that calculates a player's ICM equity 
      * @param total Total chips in play
      * @param stack Player's chip stack 
      * @param playerIndex Index of player in stacks array
-     * @param depth Current payout "depth"
+     * @param payoutIndex Current payout "payoutIndex"
      * @return The ICM equity for player
      *
      */
-    public double getEquity(int total, int stack, int playerIndex, int depth) {
-        double equity = ((double)stack / total) * this.payouts[depth];
+    public double getEquity(int total, int stack, int playerIndex, int payoutIndex) {
+        double equity = ((double)stack / total) * this.payouts[payoutIndex];
         
-        if (depth + 1 < this.numPayouts) {
+        if (payoutIndex + 1 < this.numPayouts) {
             for (int i=0; i < this.numStacks; i++) {
                 if (i != playerIndex) {
                     int currentStack = stacks[i];
                     this.stacks[i] = 0; // act as if this player busted
-                    equity += ((double)currentStack / total) * getEquity((total - currentStack), stack, playerIndex, (depth + 1));
+                    equity += ((double)currentStack / total) * getEquity((total - currentStack), stack, playerIndex, (payoutIndex + 1));
                     this.stacks[i] = currentStack; // Add "busted" player stack back in
                 }
             }
@@ -61,7 +58,7 @@ public class ICMCalculator
     
     
      /**
-     * Main calculator function
+     * Main calculator function that calls getEquity() on all players
      * @return Double array of equities of players in stack order
      */
     public double[] calculate()
@@ -74,8 +71,8 @@ public class ICMCalculator
         double[] finalEquities = new double[this.numStacks];
         
         for (int i=0; i < this.numStacks; i++) {
-            int depth = 0;
-            finalEquities[i] = getEquity(totalChips, this.stacks[i], i, depth);
+            int payoutIndex = 0;
+            finalEquities[i] = getEquity(totalChips, this.stacks[i], i, payoutIndex);
         }
         
         return finalEquities;
